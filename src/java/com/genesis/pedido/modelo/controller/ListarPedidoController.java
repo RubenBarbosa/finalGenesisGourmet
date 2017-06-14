2463993d2efe2bd80ffb7d01e5d930764b16a7d6
ruben <ruben@Lenovo-G40-80>
2017-06-14 17:07:51 -0500

    Controler de Pedido
    Proyecto GENESIS GOURMET

--- src/java/com/genesis/pedido/modelo/controller/ListarPedidoController.java
+++ src/java/com/genesis/pedido/modelo/controller/ListarPedidoController.java
@@ -5,10 +5,70 @@
  */
 package com.genesis.pedido.modelo.controller;
 
+import com.genesis.modelo.controller.util.MessageUtil;
+import com.genesis.modelo.entities.Pedido;
+import com.genesis.modelo.facade.PedidoFacadeLocal;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+
 /**
  *
  * @author ruben
  */
-public class ListarPedidoController {
+@Named(value = "listarPedidoController")
+public class ListarPedidoController implements Serializable {
+
+    @EJB
+    private PedidoFacadeLocal pfl;
+
+    private List<Pedido> pedidos;
+
+    private Pedido pedidoSeleccionado;
+
+    public ListarPedidoController() {
+    }
+    
+    @PostConstruct
+    private void init(){
+        recargarPedidos();
+    }
+
+    private void recargarPedidos() {
+        pedidos = pfl.findAll();
+    }
+
+    public PedidoFacadeLocal getPfl() {
+        return pfl;
+    }
+
+    public void setPfl(PedidoFacadeLocal pfl) {
+        this.pfl = pfl;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public Pedido getPedidoSeleccionado() {
+        return pedidoSeleccionado;
+    }
+
+    public void setPedidoSeleccionado(Pedido pedidoSeleccionado) {
+        this.pedidoSeleccionado = pedidoSeleccionado;
+    }
+    
+    public void eliminarPedido(){
+        pfl.remove(pedidoSeleccionado);
+        recargarPedidos();
+        MessageUtil.enviarMensajeInformacion("listar-pedidos", "Eliminaci�n correcta", "El pedido se ha eliminado con exito");
+    }
+    
     
 }
--- src/java/com/genesis/pedido/modelo/controller/ModificarPedidoController.java
+++ src/java/com/genesis/pedido/modelo/controller/ModificarPedidoController.java
@@ -5,10 +5,55 @@
  */
 package com.genesis.pedido.modelo.controller;
 
+import com.genesis.modelo.controller.util.MessageUtil;
+import com.genesis.modelo.entities.Pedido;
+import com.genesis.modelo.facade.PedidoFacadeLocal;
+import javax.ejb.EJB;
+import javax.inject.Named;
+
 /**
  *
  * @author ruben
  */
+@Named(value = "modificarPedidoController")
 public class ModificarPedidoController {
+    
+    @EJB
+    private PedidoFacadeLocal pfl;
+    
+    private Pedido pedidoSeleccionado;
+
+    public ModificarPedidoController() {
+    }
+
+    public PedidoFacadeLocal getPfl() {
+        return pfl;
+    }
+
+    public void setPfl(PedidoFacadeLocal pfl) {
+        this.pfl = pfl;
+    }
+
+    public Pedido getPedidoSeleccionado() {
+        return pedidoSeleccionado;
+    }
+
+    public void setPedidoSeleccionado(Pedido pedidoSeleccionado) {
+        this.pedidoSeleccionado = pedidoSeleccionado;
+    }
+    
+    public void actualizarPedido(){
+        try {
+            pfl.edit(pedidoSeleccionado);
+            MessageUtil.enviarMensajeInformacion("listar-pedidos", "Actualizaci�n", "Se han actualizado correctamente los datos");
+        } catch (Exception e) {
+            MessageUtil.enviarMensajeErrorGlobal("Error al modificar los datos", e.getStackTrace().toString());
+        }
+    }
+    
+    public String preModificar(Pedido p){
+        setPedidoSeleccionado(p);
+        return "/app/administrador/pedidos/listar.xhtml?faces-redirect=true";
+    }
     
 }
